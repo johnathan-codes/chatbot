79bc2035f85d351a0a29d8d666313c49916f5601
Andrej Šebeň <andrej.seben@millennium.sk>
2019-09-30 12:39:26 +0200

    Fixed heroku. From cwaas

--- server.js
+++ server.js
@@ -1,20 +1,27 @@
 const express = require('express');
-const http = require('http');
 const mongoose = require('mongoose');
+const path = require('path');
 const config = require('./config/keys');
-require('dotenv').config();
-const bodyParser = require('body-parser');
 const app = express();
+app.use(express.json());
 
-const PORT = process.env.PORT || 5000;
 const db = config.DB_URI;
 
-mongoose.connect(db, { useNewUrlParser: true, useUnifiedTopology: true });
+mongoose.set('useFindAndModify', false);
+mongoose
+  .connect(db, { useNewUrlParser: true, useCreateIndex: true })
+  .then(() => console.log('Database connection successful.'))
+  .catch(err => console.log(err));
 
-app.use(bodyParser.json());
+if (process.env.NODE_ENV === 'production') {
+  app.use(express.static('client/build'));
 
+  app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'));
+  });
+}
+
+const port = process.env.PORT || 5000;
 require('./routes/dialogflowRoutes')(app);
 
-app.listen(PORT, () => {
-  console.log(`Server running on port: ${PORT}`);
-});
+app.listen(port, () => console.log(`Server is running on port ${port}`));
